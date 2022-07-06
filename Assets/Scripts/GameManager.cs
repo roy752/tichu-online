2aca9b8ad8971a886eedaa804b7aa1e32e57f236
roy752 <chun_sungwon@naver.com>
2022-07-06 13:05:37 +0900

    [chun] 해상도 고정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,28 +9,30 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        float initialCardPosition = -7.0f;
+        float initialCardPosition = -1.6f;
         float heightFactor = -0.01f;
         float summonPositionX = initialCardPosition;
-        float summonPositionY = -2.7f;
+        float summonPositionY = -2.9f;
         float summonPositionZ = -1f;
         int idx = 0;
-        float cardScaleFactor = 0.30f;
+        float cardScaleFactor = 0.2f;
         Vector3 initialCardScale = new Vector3(cardScaleFactor,cardScaleFactor, cardScaleFactor);
         Quaternion initialCardRotation = Quaternion.Euler(270f, 180f, 180f);
         foreach(var item in cards)
         {
-            //if ((idx - 1) / 7 != idx / 7)
+            if ((idx - 1) / 7 != idx / 7)
             {
-              //  summonPositionY += 2.7f;
-              //  summonPositionX = initialCardPosition;
+                summonPositionY -= 0.9f;
+                summonPositionX = initialCardPosition;
             }
             ++idx;
             var obj = Instantiate(item, new Vector3(summonPositionX, summonPositionY, summonPositionZ), initialCardRotation, transform);
             obj.transform.localScale = initialCardScale;
             summonPositionZ += heightFactor;
-            summonPositionX += 1.1f;
+            summonPositionX += 0.52f;
         }
+        var obje =  Instantiate(cards[0], new Vector3(summonPositionX, summonPositionY, summonPositionZ), initialCardRotation, transform);
+        obje.transform.localScale = initialCardScale;
     }
 
     // Update is called once per frame
